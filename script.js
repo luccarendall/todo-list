16a979b982353277a35af18769921eb5a831222e
Lucca Rendall <luccarendall1@gmail.com>
2022-01-19 18:13:10 -0300

    Corrigindo erros do ESLint

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-  const createTaskButton = document.getElementById('criar-tarefa');
+const createTaskButton = document.getElementById('criar-tarefa');
 
-    /* No momento que houver o clique no 'adicionar', a função será ativada */
-    createTaskButton.addEventListener('click', function () { 
-    const taskName = document.getElementById('texto-tarefa');
-    const lista = document.getElementById('lista-tarefas');
-    const task = document.createElement('li');
-    task.innerText = taskName.value;
-    lista.appendChild(task);
-    taskName.value = '';
-  });
+/* No momento que houver o clique no 'adicionar', a função será ativada */
+createTaskButton.addEventListener('click', () => {
+  const taskName = document.getElementById('texto-tarefa');
+  const lista = document.getElementById('lista-tarefas');
+  const task = document.createElement('li');
+  task.innerText = taskName.value;
+  lista.appendChild(task);
+  taskName.value = '';
+});

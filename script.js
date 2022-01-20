4b5f5e3bc9ee9d3c1f85bb0a2f380b9826df7553
Lucca Rendall <luccarendall1@gmail.com>
2022-01-20 05:11:11 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
+// Função para criar botão de adicionar nova task
 const createTaskButton = document.getElementById('criar-tarefa');
-
-/* No momento que houver o clique no 'adicionar', a função será ativada */
 createTaskButton.addEventListener('click', () => {
   const taskName = document.getElementById('texto-tarefa');
   const listaOrdenada = document.getElementById('lista-tarefas');
@@ -10,10 +9,19 @@ createTaskButton.addEventListener('click', () => {
   taskName.value = '';
 });
 
-// Pq não funciona com arrow function?
+// Função para mudar cor do background para cinza (Off: Procurar como usar arrow function aqui)
 const listaOrdenada = document.getElementById('lista-tarefas');
 function liGrayBackground() {
   // eslint-disable-next-line no-restricted-globals
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 listaOrdenada.addEventListener('click', liGrayBackground);
+
+// Função para criar botão "Limpar"
+const createClearButton = document.getElementById('apaga-tudo');
+createClearButton.addEventListener('click', () => {
+  listaOrdenada.innerHTML = '';
+});
+
+// const createCompleteTaskButton = document.getElementById('remover-finalizados');
+// //inserir função de clicar botão remover concluídos.
